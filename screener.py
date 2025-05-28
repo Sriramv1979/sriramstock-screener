4a2a3781851dfacbc446e1a152ebc8abde56633c
Sriramv1979 <sriram@smbenablers.com>
2025-05-28 13:40:22 +0530

    Delete screener.py

--- screener.py
+++
@@ -1,5 +0,0 @@
-def is_long_entry(row):
-    return row['Close'] > row['EMA50'] and row['RSI'] >= 50 and row['SAR'] < row['Close']
-
-def is_short_entry(row):
-    return row['Close'] < row['EMA50'] and row['RSI'] < 50 and row['SAR'] > row['Close']
