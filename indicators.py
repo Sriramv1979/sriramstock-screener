2fd4c40a4bbc3adfc8ea42a0e2f374b9fb5865b1
Sriramv1979 <sriram@smbenablers.com>
2025-05-28 13:40:00 +0530

    Delete indicators.py

--- indicators.py
+++
@@ -1,7 +0,0 @@
-import pandas_ta as ta
-
-def add_indicators(df):
-    df['EMA50'] = ta.ema(df['Close'], length=50)
-    df['RSI'] = ta.rsi(df['Close'], length=14)
-    df['SAR'] = ta.psar(df['High'], df['Low'], df['Close'])['PSARl_0.02_0.2']
-    return df
